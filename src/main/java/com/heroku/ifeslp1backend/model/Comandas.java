d6936cd30e12cc3d24c2dbd25b2a8c6b1c1013bd
sklwy <78670580+sklwy@users.noreply.github.com>
2021-03-24 19:06:22 -0300

    [#13] - Organização imports

--- src/main/java/com/heroku/ifeslp1backend/model/Comandas.java
+++ src/main/java/com/heroku/ifeslp1backend/model/Comandas.java
@@ -2,7 +2,17 @@ package com.heroku.ifeslp1backend.model;
 
 import com.heroku.ifeslp1backend.enumerator.EComStatus;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 import java.util.List;
 
