beae09fa52413af6854108e8cc82c70cb5774bea
sklwy <78670580+sklwy@users.noreply.github.com>
2021-03-24 19:01:54 -0300

    [#12] - Adicionado iteração na lista de Pedidos
    Correção imports

--- src/main/java/com/heroku/ifeslp1backend/model/Pedidos.java
+++ src/main/java/com/heroku/ifeslp1backend/model/Pedidos.java
@@ -2,14 +2,7 @@ package com.heroku.ifeslp1backend.model;
 
 import com.heroku.ifeslp1backend.enumerator.EPedStatus;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "GER_PEDIDOS")
